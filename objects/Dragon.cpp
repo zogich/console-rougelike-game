80fb8742aa1ea0ef6c218225d9b63c98b0ce1e91
TCKACHIKSIS <71429999+zogich@users.noreply.github.com>
2023-01-30 01:48:03 +1000

    add some more classes

---
+++ objects/Dragon.cpp
@@ -0,0 +1,20 @@
+//
+// Created by иван on 30.01.2023.
+//
+
+#include "Dragon.h"
+
+Dragon::Dragon(const Point &pos) {
+    this->SetSym('D');
+    this->SetPos(pos);
+    this->hp = 20;
+    this->damage = 2;
+}
+
+int Dragon::GetDamage() {
+    return this->damage;
+}
+
+void Dragon::TakeDamage(const int &dmg) {
+    this->hp -= dmg;
+}
